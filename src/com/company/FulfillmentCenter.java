668213b9922ee1243299810caf3c9ef3d8e445c2
Marcin Graja <36998161+trahatar@users.noreply.github.com>
2019-03-10 19:36:33 +0100

    can you please work

--- src/com/company/FulfillmentCenter.java
+++ src/com/company/FulfillmentCenter.java
@@ -7,21 +7,25 @@ import static com.company.ItemCondition.*;
 public class FulfillmentCenter {
     private String name;
     public ArrayList <Item> listOfProducts = new ArrayList<>();
-    private int capacity;
+    private double capacity;
+    private double currentCapacity;
     FulfillmentCenter(String name, int capacity){
         this.name = name;
         this.capacity=capacity;
     }
     public void addProduct(Item item){
+        if (currentCapacity<item.getCount()*item.getMass()){
+            System.err.println("Brak miejsca w magazynie; Wymagane miejsce: " + item.getCount()*item.getMass() + "; dostępne: " + currentCapacity);
+        }
         if (listOfProducts.contains(item)){
             Item listedItem = listOfProducts.get(listOfProducts.indexOf(item));
             listedItem.setCount(listedItem.getCount()+item.getCount());
-
         }
         else{
             Item newItem = new Item(item.getName(), item.getCondition(), item.getMass(), item.getCount());
             listOfProducts.add(newItem);
         }
+        currentCapacity-=item.getCount()*item.getMass();
     }
     public void getProduct(Item item){
         if (listOfProducts.indexOf(item)==-1){
@@ -29,6 +33,7 @@ public class FulfillmentCenter {
             return;
         }
         Item listedItem = listOfProducts.get(listOfProducts.indexOf(item));
+        currentCapacity+=listedItem.getMass();
         if (listedItem.getCount()==1){
             listOfProducts.remove(listedItem);
             return;
@@ -40,6 +45,8 @@ public class FulfillmentCenter {
             System.err.println("brak takiego produktu; funkcja removeProduct");
         }
         else{
+            Item listedItem = listOfProducts.get(listOfProducts.indexOf(item));
+            currentCapacity+=listedItem.getMass()*listedItem.getCount();
             listOfProducts.remove(item);
         }
     }
@@ -54,7 +61,40 @@ public class FulfillmentCenter {
         }
         return returned;
     }
-    public int getCapacity(){
+    public List searchPartial(String partialName){
+        Iterator iterator = listOfProducts.iterator();
+        List<Item> returned = new ArrayList<>();
+        while (iterator.hasNext()){
+            Item item = (Item) iterator.next();
+            if ((item.getName().contains(name))){
+                returned.add(item);
+            }
+        }
+        return returned;
+    }
+    public int[] countByCondition() {
+        Iterator iterator = listOfProducts.iterator();
+        int[] returned = new int[3];
+        while (iterator.hasNext()) {
+            Item item = (Item) iterator.next();
+            returned[item.getCondition().ordinal()] += item.getCount();
+        }
+        return returned;
+    }
+    public void summary(){
+        for(Item item: listOfProducts){
+            item.print();
+        }
+    }
+    public List sortByName(){
+        return listOfProducts.sort(new Comparator<Item>() {
+            @Override
+            public int compare(Item o1, Item o2) {
+                return o1.compareTo(o2);
+            }
+        });
+    }
+    public double getCapacity(){
         return capacity;
     }
     public void print(){
--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,4 +1,7 @@
 package com.company;
+
+import java.util.Comparator;
+
 enum ItemCondition{
     NEW, USED, REFURBISHED;
 }
